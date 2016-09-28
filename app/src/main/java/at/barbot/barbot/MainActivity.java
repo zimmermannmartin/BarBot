3b753865c3431944b3c9593a30684d327a156c8a
Martin Zimmermann <martin.zimmermann19@gmail.com>
2016-09-28 21:14:23 +0200

    Changed menu, added menu icons

--- app/src/main/java/at/barbot/barbot/MainActivity.java
+++ app/src/main/java/at/barbot/barbot/MainActivity.java
@@ -80,9 +80,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.bar_list) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.add_drink) {
 
         } else if (id == R.id.nav_slideshow) {
 
