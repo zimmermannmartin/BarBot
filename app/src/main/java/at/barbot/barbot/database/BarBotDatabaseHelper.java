a007532a3209c8b00b03288d3b8b4085b02871b0
Martin Zimmermann <martin.zimmermann19@gmail.com>
2016-10-22 18:26:13 +0200

    added update Operations to BarBotDatabaseHelper

--- app/src/main/java/at/barbot/barbot/database/BarBotDatabaseHelper.java
+++ app/src/main/java/at/barbot/barbot/database/BarBotDatabaseHelper.java
@@ -2,6 +2,7 @@ package at.barbot.barbot.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -222,6 +223,103 @@ public class BarBotDatabaseHelper extends SQLiteOpenHelper {
             db.setTransactionSuccessful();
         }catch (Exception e){
             Log.d(TAG, "Error adding a new Drink: " + e);
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    public void updateBarBot (BarBot barBot){
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_BARBOT_PK_ID_BARBOT, barBot.pk_id_barbot);
+            values.put(COLUMN_BARBOT_NAME, barBot.name);
+
+            int rows = db.update(TABLE_BARBOT, values, COLUMN_BARBOT_PK_ID_BARBOT + "= ?", new String[]{""+barBot.pk_id_barbot});
+            db.setTransactionSuccessful();
+        }catch (Exception e){
+            Log.d(TAG, "Error updating the BarBot" + e);
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    public void updateSlaveUnit (Slaveunit slave){
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_SLAVEUNIT_PK_ID_SLAVEUNIT, slave.pk_id_slaveunit);
+            values.put(COLUMN_SLAVEUNIT_NAME, slave.name);
+            values.put(COLUMN_SLAVEUNIT_FILLING_LEVEL_IN_ML, slave.filling_level_in_ml);
+            values.put(COLUMN_SLAVEUNIT_FK_ID_BARBOT, slave.fk_id_barbot);
+            values.put(COLUMN_SLAVEUNIT_FK_ID_INGREDIENT, slave.fk_id_ingredient);
+
+            int rows = db.update(TABLE_SLAVEUNIT, values, COLUMN_SLAVEUNIT_PK_ID_SLAVEUNIT + "= ?", new String[]{""+slave.pk_id_slaveunit});
+            db.setTransactionSuccessful();
+        }catch (Exception e){
+            Log.d(TAG, "Error updating Slaveunit" + e);
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    public void updateIngredient (Ingredient ingredient){
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_INGREDIENT_PK_ID_INGREDIENT, ingredient.pk_id_ingredient);
+            values.put(COLUMN_INGREDIENT_NAME, ingredient.name);
+            values.put(COLUMN_INGREDIENT_VOL_PERCENT, ingredient.vol_percent);
+
+            int rows = db.update(TABLE_INGREDIENT, values, COLUMN_INGREDIENT_PK_ID_INGREDIENT + "= ?", new String[]{""+ingredient.pk_id_ingredient});
+            db.setTransactionSuccessful();
+        }catch (Exception e){
+            Log.d(TAG, "Error updating Ingredient" + e);
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    public void updateDrinkHasIngredient (Drink_has_ingredient drinkHasIngredient){
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_DRINK_HAS_INGREDIENT_PK_FK_ID_DRINK, drinkHasIngredient.pk_fk_id_drink);
+            values.put(COLUMN_DRINK_HAS_INGREDIENT_PK_FK_ID_INGREDIENT, drinkHasIngredient.pk_fk_id_ingredient);
+            values.put(COLUMN_DRINK_HAS_INGREDIENT_INGREDIENT_AMOUNT_IN_ML, drinkHasIngredient.ingredient_amount_in_ml);
+
+            int rows = db.update(TABLE_DRINK_HAS_INGREDIENT, values, COLUMN_DRINK_HAS_INGREDIENT_PK_FK_ID_DRINK + "= ? AND " + COLUMN_DRINK_HAS_INGREDIENT_PK_FK_ID_DRINK + "= ?", new String[]{""+drinkHasIngredient.pk_fk_id_drink, ""+drinkHasIngredient.pk_fk_id_ingredient});
+            db.setTransactionSuccessful();
+        }catch (Exception e){
+            Log.d(TAG, "Error updating Drink_has_Ingredient" + e);
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    public void updateDrink (Drink drink){
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+        try {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_DRINK_PK_ID_DRINK, drink.pk_id_drink);
+            values.put(COLUMN_DRINK_NAME, drink.name);
+            values.put(COLUMN_DRINK_DESCRIPTION, drink.description);
+            values.put(COLUMN_DRINK_PICTURE, drink.picture);
+
+            int rows = db.update(TABLE_DRINK, values, COLUMN_DRINK_PK_ID_DRINK + "= ?", new String[]{""+drink.pk_id_drink});
+            db.setTransactionSuccessful();
+        }catch (Exception e){
+            Log.d(TAG, "Error updating Drink" + e);
         }finally {
             db.endTransaction();
         }
